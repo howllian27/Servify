6778300e65f1612f11fa673c9d099f73f4101bac
Howllian27 <67510209+howllian27@users.noreply.github.com>
2023-10-24 21:06:44 +0800

    added countdown timer

--- Frontend/templates/script/confirmation.js
+++ Frontend/templates/script/confirmation.js
@@ -19,3 +19,28 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector(".green-box2 .text-container p:nth-child(4)").textContent = `Location: ${location} `;
     }
 });
+
+// Function to start the countdown timer
+function startCountdown() {
+    let countdown = 20; // Start countdown from 20 seconds
+
+    // Display the countdown modal
+    $('#countdownModal').modal('show');
+
+    // Update the countdown every second
+    const interval = setInterval(function() {
+        countdown--;
+
+        // Update the countdown value in the modal
+        document.getElementById('countdown').innerText = countdown;
+
+        // If countdown reaches 0, redirect to index.html and clear the interval
+        if (countdown <= 0) {
+            clearInterval(interval);
+            window.location.href = "./index.html";
+        }
+    }, 1000);
+}
+
+// Start the countdown timer when the page loads
+window.onload = startCountdown;
